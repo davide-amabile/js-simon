a033445b2cf368b213080a64b67bd874812ade31
davide-amabile <davideamabile81@gmail.com>
2020-09-04 17:33:53 +0200

    inserimento numeri con controllo

--- js/script.js
+++ js/script.js
@@ -19,3 +19,66 @@ function numRdnFrom1 (min, max, numRdn) {
 }
   return lista;
 }
+
+// chiedere 5 numeri all'utente dopo 30 secondi chiuso l'allert
+
+var numUtente = [];
+
+var i = 0;
+while ( i < 5 ) {
+  var numIns= parseInt(prompt("Inserisci un numero"));
+  if ( isNaN(numIns) || numIns < 1 || numIns > 100  ) {
+    alert("inserire un numero da 1 a 100");
+  } else {
+    numUtente.push(numIns);
+      i++;
+  }
+}
+console.log(numUtente);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numUtente = [];
+//
+//   var i= 0;
+//
+//   do{
+//     var numCheck = [];
+//     var numIns = parseInt(prompt("Inserire un numero"));
+//     // controlli numeri inseriti
+//     if ( isNaN(numIns) ) {
+//       alert("inserire un numero da 1 a 100")
+//       numCheck.splice(numIns);
+//     } else {
+//       numCheck.push(numIns);
+//     }
+//     i++;
+//   } while ( i < 5);
+//   console.log(numCheck);
+//   console.log(numUtente);
