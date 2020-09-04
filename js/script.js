51120a486be04bb93702cf513b7cc1116e331f9a
davide-amabile <davideamabile81@gmail.com>
2020-09-04 16:05:18 +0200

    generato 5 numeri random non uguali

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("");
+// creare allert con 5 numeri
+var numRdm = numRdnFrom1(1, 100, 5);
+console.log(numRdm);
+
+alert(numRdm);
+
+// generare 5 numeri random
+
+
+function numRdnFrom1 (min, max, numRdn) {
+  var lista = [];
+  var i = 1;
+  while ( i <= numRdn) {
+  var numRandom =Math.floor(Math.random() * (max - min + 1) ) + min;
+  if ( lista.indexOf(numRandom) === -1 ) {
+    lista.push(numRandom);
+  i++;
+  }
+}
+  return lista;
+}
