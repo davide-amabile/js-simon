12e8240256958e96c9d88fd73b8a67706598fe10
davide-amabile <davideamabile81@gmail.com>
2020-09-05 19:29:08 +0200

    controllo quanti e quali numeri indovinati

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// creare allert con 5 numeri
+//1.creare allert con 5 numeri
 var numRdm = numRdnFrom1(1, 100, 5);
-console.log(numRdm);
 
 alert(numRdm);
 
-// generare 5 numeri random
+//2.generare 5 numeri random
 
 
 function numRdnFrom1 (min, max, numRdn) {
@@ -20,25 +19,63 @@ function numRdnFrom1 (min, max, numRdn) {
   return lista;
 }
 
-// chiedere 5 numeri all'utente dopo 30 secondi chiuso l'allert
-setTimeout( myFunction, 5000);
+//3. chiedere 5 numeri all'utente dopo 30 secondi chiuso l'allert
+
+setTimeout( myFunction, 30000);
 
 function myFunction() {
-var numUtente = [];
 
-var i = 0;
-while ( i < 5 ) {
+// array numeri uguali
+ var x = [];
+
+
+// array numeri diversi
+var y = [];
+
+
+
+for ( i = 0; i < 5; i++) {
   var numIns= parseInt(prompt("Inserisci un numero"));
   if ( isNaN(numIns) || numIns < 1 || numIns > 100  ) {
     alert("inserire un numero da 1 a 100");
-  } else {
-    numUtente.push(numIns);
-      i++;
+  } else  {
+    // for ( c = 0; c < 5; c++ ) {
+
+    var checkStato = checkDatiIns( numRdm, numIns);
+    if ( checkStato == true) {
+      x.push(numIns);
+    }else {
+      y.push(numIns);
+    }
+   // }
+  }
+
+
+// funzione per controllare se il numero è presente nell'array
+  function checkDatiIns(array, dato) {
+  var check = true;
+  var i = 0;
+  while ( i < array.length) {
+    if (array.indexOf(dato) == -1) {
+      check = false;
+    }
+    i++;
+    return check
   }
 }
-console.log(numUtente);
+}
+
+// verificare quanti e quali numeri hanno indovinato
+console.log("Hai indovinato" + " " + x.length + " " +  "numeri:" + " " + x);
+console.log("Hai sbagliato" + " " + y.length + " " +  "numeri:" + " " + y);
+
 
 }
+
+
+
+
+
 
 
 
