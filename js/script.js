b6e889695508a9ade2dc75d2473787b8c7d903c7
davide-amabile <davideamabile81@gmail.com>
2020-09-05 19:44:02 +0200

    controllo quanti e quali numeri indovinati o sbagliati

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 //1.creare allert con 5 numeri
 var numRdm = numRdnFrom1(1, 100, 5);
-
 alert(numRdm);
 
 //2.generare 5 numeri random
-
-
 function numRdnFrom1 (min, max, numRdn) {
   var lista = [];
   var i = 1;
@@ -20,38 +17,32 @@ function numRdnFrom1 (min, max, numRdn) {
 }
 
 //3. chiedere 5 numeri all'utente dopo 30 secondi chiuso l'allert
-
-setTimeout( myFunction, 30000);
+setTimeout( myFunction, 5000);
 
 function myFunction() {
-
 // array numeri uguali
  var x = [];
-
-
 // array numeri diversi
 var y = [];
-
-
-
-for ( i = 0; i < 5; i++) {
+//4. controllo numeri inseriti
+var i = 0;
+while (  i < 5 ) {
   var numIns= parseInt(prompt("Inserisci un numero"));
   if ( isNaN(numIns) || numIns < 1 || numIns > 100  ) {
     alert("inserire un numero da 1 a 100");
   } else  {
-    // for ( c = 0; c < 5; c++ ) {
-
     var checkStato = checkDatiIns( numRdm, numIns);
     if ( checkStato == true) {
       x.push(numIns);
+      i++;
     }else {
-      y.push(numIns);
+      y.push(numIns)
+      i++;
     }
-   // }
   }
 
 
-// funzione per controllare se il numero è presente nell'array
+//6. funzione per controllare se il numero è presente nell'array
   function checkDatiIns(array, dato) {
   var check = true;
   var i = 0;
@@ -65,11 +56,9 @@ for ( i = 0; i < 5; i++) {
 }
 }
 
-// verificare quanti e quali numeri hanno indovinato
+//7. verificare quanti e quali numeri hanno indovinato
 console.log("Hai indovinato" + " " + x.length + " " +  "numeri:" + " " + x);
 console.log("Hai sbagliato" + " " + y.length + " " +  "numeri:" + " " + y);
-
-
 }
 
 
